e39c1ee4410a25e79706491c25b2a12d95032c67
Sunny <sundeepkmanik@gmail.com>
2018-03-29 21:17:13 -0700

    added js stuff

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+    // ================== GLOBAL VARIABLES
+    // ===================================
+
+    var searchInput;
+    var recordInput;
+    var startYearInput;
+    var endYearInput;
+
+    $(".submit-button").on("click", function() {
+
+
+
+    });
+
+// document closing tag don't touch
+});
